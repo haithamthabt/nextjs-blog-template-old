e2dea8e5b12b1d495dd1e605c0e04c0170c45aea
Haitham Thabt <haithamthabt@hotmail.com>
2022-04-29 04:07:56 -0400

    added layout.js file to manage site layout

---
+++ nextjs-blog-template/components/Layout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+export default function Layout({title, keywords, description, children}) {
+  return (
+    <div>
+        <Head>
+            <title>{title}</title>
+            <meta name='keywords' content={keywords}></meta>
+            <meta name='description' content={description}></meta>
+            <link rel='icon' href='/favicon.ico'></link>
+        </Head>
+        <main className='container mx-auto my-7'>{children}</main>
+    </div>
+  )
+}
+
+
+
+// add some defaults 
+Layout.defaultProps = {
+    title: "Welcome to Nextjs.net",
+    keywords: "Tutorials, Next,js, Nextjs, next js",
+    description: "The best Next.js tuturials"
+
+}
