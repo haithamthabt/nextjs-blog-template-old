e6bd22a9181ff9dd6fa3503920276eabe4e28038
Haitham Thabt <haithamthabt@hotmail.com>
2022-04-29 18:43:20 -0400

    fixed padding in about page, issue was instead of pb I wrote bp

--- nextjs-blog-template/pages/about.js
+++ nextjs-blog-template/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 export default function AboutPage() {
   return (
     <Layout title='About Nextjs.net'>
-        <h1 className="text-5xl border-b-4 pd-5 font-bold">About</h1>
+        <h1 className='text-5xl border-b-4 pb-5 font-bold'>About</h1>
 
         <div className="bg-white shadow-md rounded-lg px-10 py-6 mt6">
             <h3 className="text-2xl mb-5">Nextjs.net</h3>
