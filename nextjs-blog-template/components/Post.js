2504cbb956633859c2164e8d9595bac5b0dea860
Haitham Thabt <haithamthabt@hotmail.com>
2022-04-30 16:30:39 -0400

    Replaced the Next Image tag with html img tag since export does not support Next Image

--- nextjs-blog-template/components/Post.js
+++ nextjs-blog-template/components/Post.js
@@ -1,11 +1,10 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import CategoryLabel from './CategoryLabel'
 
 export default function Post({ post }) {
   return (
     <div className='w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
-      <Image
+      <img
         src={post.frontmatter.cover_image}
         alt=''
         height={420}
