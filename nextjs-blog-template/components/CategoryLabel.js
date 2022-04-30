5c3a98c6aeeea3157ff3418f6ca17b787cf1447c
Haitham Thabt <haithamthabt@hotmail.com>
2022-04-30 14:42:33 -0400

    added the categorylable compenent

---
+++ nextjs-blog-template/components/CategoryLabel.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+export default function CategoryLabel({ children }) {
+  const colorKey = {
+    JavaScript: "yellow",
+    CSS: "blue",
+    Python: 'green',
+    PHP: 'purple',
+    Ruby: 'red',
+  }
+  //const bg_color = "bg-"+colorKey[children]+"-600"
+  const bg_color = "bg-gray-600"
+  return (
+    <div
+      className={`px-2 py-1 ${bg_color} text-gray-100 font-bold rounded`}
+    >
+      <Link href={`/blog/category/${children.toLowerCase()}`}>{children}</Link>
+    </div>
+  )
+}
