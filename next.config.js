56e71ad616ab24ba3f781170c9840ace1511ae49
Haitham Thabt <haithamthabt@hotmail.com>
2022-05-05 12:18:44 -0400

    fixed /blog/page not found

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/blog/page',
+        destination: '/blog/page/1',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
