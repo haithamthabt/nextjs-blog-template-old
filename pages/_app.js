fdb9633b1b419e8faa50955af06be81c3f86c89e
Haitham Thabt <haithamthabt@hotmail.com>
2022-05-08 16:55:39 -0400

    no need for custom markdown styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import '../styles/markdown_styles.scss'
+//import '../styles/markdown_styles.scss'
 import "prismjs/themes/prism-tomorrow.css";
 
 function MyApp({ Component, pageProps }) {
