09988114045d425d9d1a9b208f1fc834fe3f783a
Haitham Thabt <haithamthabt@hotmail.com>
2022-05-12 12:50:24 -0400

    moved images to assets folder and fixed routing in code

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
         <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
             <Link href='/'>
                 <a className='flex md:w-1/5 title-font font-medium items-center md:justify-start mb-4 md:mb-0'>
-                    <img src='/images/logo.png' width={40} height={40} alt='logo' />
+                    <img src='/assets/images/logo.png' width={40} height={40} alt='logo' />
                     <span className='ml-3 text-xl'>Nextjs.net</span>
                 </a>
             </Link>
