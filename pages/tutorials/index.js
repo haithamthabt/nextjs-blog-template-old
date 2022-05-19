b5f914ad68e91baa668236b0060f37080ebf4b17
Haitham Thabt <haithamthabt@hotmail.com>
2022-05-19 01:47:36 -0400

    created function to get tutorials

--- pages/tutorials/index.js
+++ pages/tutorials/index.js
@@ -1,11 +1,34 @@
+import Layout from '@/components/Layout';
+import Link from 'next/link';
+import { getTutorials } from '@/utils/tutorials';
+
+export default function TutorialsPage({tutorials}) {
 
 
-export default function TutorialsPage() {
+  console.log("ttttttttttttttttttttttttttttttttttttttttttttttttttttt")
+  console.log(tutorials)
   return (
-    <div>This is the home page for tutorials</div>
+    <Layout>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Tutorials</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {tutorials.map((tutorial, index) => (
+          <Link href={`tutorials/css/${tutorial.slug}`}>
+          {tutorial.frontmatter.title}
+          </Link>
+        )) }
+      </div>
+    </Layout>
   )
 }
 
+export async function getStaticProps(){
 
+  console.log("I am here ------------------------------")
+  console.log(getTutorials())
 
-
+  return {
+      props: {
+          tutorials: getTutorials(),
+      },
+  }
+}
