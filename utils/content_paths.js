446065f68ba12770d1a01b929f703c4ca34efe84
Haitham Thabt <haithamthabt@hotmail.com>
2022-05-20 04:00:34 -0400

    created path for author

--- utils/content_paths.js
+++ utils/content_paths.js
@@ -2,3 +2,5 @@ export const BLOG_POSTS_PATH = String("_data/content/blog_posts");
 export const ABOUT_CONTENT_PATH = String("_data/about");
 export const CONTACT_CONTENT_PATH = String("_data/contact");
 export const TUTORIALS_PATH = String("_data/content/tutorials");
+export const AUTHORS_PATH = String("_data/content/authors");
+
