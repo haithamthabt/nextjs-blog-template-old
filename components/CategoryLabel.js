5720d70fa2bd5a8ac34003c3fee965d3a896aa1f
Haitham Thabt <haithamthabt@hotmail.com>
2022-05-21 03:41:16 -0400

    moved category color to config/settings

--- components/CategoryLabel.js
+++ components/CategoryLabel.js
@@ -1,17 +1,11 @@
 import Link from 'next/link'
+import { BLOG_CATEGORY_COLOR_KEY } from "@_data/config/settings";
 
 export default function CategoryLabel({ children }) {
-  const colorKey = {
-    JavaScript: "bg-yellow-600",
-    CSS: "bg-blue-600",
-    Python: 'bg-green-600',
-    PHP: 'bg-purple-600',
-    Ruby: 'bg-red-600',
-  }
-  //const bg_color = "bg-"+colorKey[children]+"-600"
+
   return (
     <div
-      className={`px-2 py-1 ${colorKey[children]} text-gray-100 font-bold rounded`}
+      className={`px-2 py-1 ${BLOG_CATEGORY_COLOR_KEY[children]} text-gray-100 font-bold rounded`}
     >
       <Link href={`/blog/category/${children.toLowerCase()}`}>{children}</Link>
     </div>
